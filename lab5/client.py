79e6ff590da6fe70d61efa20b86e28d7ca21c9d3
asrafulashiq <ashrafulislam@ash-mac.home>
2019-03-29 02:14:21 -0400

    /

--- lab5/client.py
+++ lab5/client.py
@@ -1,10 +1,16 @@
 import socket
+import sys
 
 serverName = 'enigma-decoder.local'
 serverPort = 31319
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
+
+try:
+    clientSocket.connect((serverName, serverPort))
+except socket.error as msg:
+    print('Connection failed: ' + msg[1])
+    sys.exit()
 
 sentence = input("Input lowercase sentence:")
 clientSocket.send(sentence)
